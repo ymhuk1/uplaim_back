be135b1b598364ee5bf587f4809dc0b4f7cd7ef7
viktor <viktorkalakun@gmail.com>
2024-07-23 00:06:41 +0500

    del push2

--- utils/push_sender.py
+++ utils/push_sender.py
@@ -1,21 +1,21 @@
-import requests
-
-from models import Push
-
-
-async def send_notification(client, title, body, session):
-    url = "https://exp.host/--/api/v2/push/send"
-    headers = {
-        "Accept": "application/json",
-        "Accept-Encoding": "gzip, deflate",
-        "Content-Type": "application/json",
-    }
-    payload = {
-        "to": client.push_token,
-        "title": title,
-        "body": body,
-    }
-    response = requests.post(url, json=payload, headers=headers)
-    new_push = Push(to=client.push_token, title=title, body=body, data=response.json(), client=client)
-
-    await session.add(new_push)
+# import requests
+#
+# from models import Push
+#
+#
+# async def send_notification(client, title, body, session):
+#     url = "https://exp.host/--/api/v2/push/send"
+#     headers = {
+#         "Accept": "application/json",
+#         "Accept-Encoding": "gzip, deflate",
+#         "Content-Type": "application/json",
+#     }
+#     payload = {
+#         "to": client.push_token,
+#         "title": title,
+#         "body": body,
+#     }
+#     response = requests.post(url, json=payload, headers=headers)
+#     new_push = Push(to=client.push_token, title=title, body=body, data=response.json(), client=client)
+#
+#     await session.add(new_push)
